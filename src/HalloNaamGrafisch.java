0898698aa808ab967057d31ea2dbddd7e88b2eb1
christelvanharen <cvanharen@outlook.com>
2020-09-11 12:34:14 +0200

    Afvink 1

---
+++ src/HalloNaamGrafisch.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class HalloNaamGrafisch {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello " + args[0]);
+    }
+}
---
+++ src/HelloNaam.java
@@ -0,0 +1,6 @@
+public class HelloNaam {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo " + args[0]);
+    }
+}
