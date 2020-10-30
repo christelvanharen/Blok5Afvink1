2f5c33158bd2b24b36f53aa6b87dd5767a46c8ff
christelvanharen <cvanharen@outlook.com>
2020-10-30 16:53:20 +0100

    Afvink 7

--- src/Afvink_7/SeqVis/Gui.java
+++ src/Afvink_7/SeqVis/Gui.java
@@ -46,122 +46,116 @@ public class Gui {
         tekenpaneel.add(paneel);
         tekenpaneel.setVisible(true);
 
-        kiesEenBestandButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                /**
-                 * Het bestand kan worden geopend
-                 */
-                File selectedFile;
-                JFileChooser fileChooser = new JFileChooser();
-                int reply = fileChooser.showOpenDialog(null);
-                if (reply == JFileChooser.APPROVE_OPTION) {
-                    selectedFile = fileChooser.getSelectedFile();
-                    bestandskiezer.setText(((File) selectedFile).getAbsolutePath());
-                }
+        kiesEenBestandButton.addActionListener(e -> {
+            /**
+             * Het bestand kan worden geopend
+             */
+            File selectedFile;
+            JFileChooser fileChooser = new JFileChooser();
+            int reply = fileChooser.showOpenDialog(null);
+            if (reply == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+                bestandskiezer.setText(((File) selectedFile).getAbsolutePath());
             }
         });
 
-        visualiserenButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                /**
-                 * Controleert of het DNA, RNA of eiwit is
-                 */
-                int jeiwit = 0;
-                int jDNA = 0;
-                int jRNA = 0;
-                String seq = null;
+        visualiserenButton.addActionListener(e -> {
+            /**
+             * Controleert of het DNA, RNA of eiwit is
+             */
+            int jeiwit = 0;
+            int jDNA = 0;
+            int jRNA = 0;
+            String seq = null;
 
-                String line;
-                int t = 0;
+            String line;
+            int t = 0;
 
-                try {
-                    BufferedReader inFile = new BufferedReader(new FileReader(bestandskiezer.getText()));
-                    while ((line = inFile.readLine()) != null) {
-                        if (seq == null) {
-                            seq = line.toString();
-                        } else {
-                            seq += line.toString();
+            try {
+                BufferedReader inFile = new BufferedReader(new FileReader(bestandskiezer.getText()));
+                while ((line = inFile.readLine()) != null) {
+                    if (seq == null) {
+                        seq = line.toString();
+                    } else {
+                        seq += line.toString();
+                    }
+                    for (int i = 0; i < line.length(); i++) {
+                        t++;
+
+                        char a = line.charAt(i);
+                        System.out.println(a);
+                        if (a == 'A' || a == 'G' || a == 'T' || a == 'C') {
+                            jDNA++;
                         }
-                        for (int i = 0; i < line.length(); i++) {
-                            t++;
-
-                            char a = line.charAt(i);
-                            System.out.println(a);
-                            if (a == 'A' || a == 'G' || a == 'T' || a == 'C') {
-                                jDNA++;
-                            }
-                            if (a == 'A' || a == 'G' || a == 'U' || a == 'C') {
-                                jRNA++;
-                            }
-                            for (int x = 0; x < ONE.length; x++) {
-                                char s = ONE[x].charAt(0);
-                                if (a == s) {
-                                    jeiwit++;
-                                    if (x > 11) {
-                                    } else {
-                                    }
+                        if (a == 'A' || a == 'G' || a == 'U' || a == 'C') {
+                            jRNA++;
+                        }
+                        for (int x = 0; x < ONE.length; x++) {
+                            char s = ONE[x].charAt(0);
+                            if (a == s) {
+                                jeiwit++;
+                                if (x > 11) {
+                                } else {
                                 }
                             }
                         }
                     }
-
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
                 }
 
-                if (jDNA == t) {
-                    textArea.setText("DNA: " + seq);
-                    DNA d1 = new DNA();
-                    assert seq != null;
-                    d1.setgcpers(seq);
-                    textArea.setText("Het GC-percentage is " + d1.getgcpers() + "%");
-                    System.out.println(d1.getgcpers());
-
-                    Graphics tekenveld = tekenpaneel.getGraphics();
-                    int afstand = 50;
-                    for (int i = 0; i < seq.length(); i++) {
-                        char a = seq.charAt(i);
-                        d1.setcolor(a);
-                        tekenveld.setColor(d1.getColor());
-                        tekenveld.drawRect(afstand, 50, 10, 20);
-                        tekenveld.fillRect(afstand, 50, 10, 20);
-                        afstand += 10;
-                    }
-                } else if (jRNA == t) {
-                    textArea.setText("RNA: " + seq);
-                    RNA r1 = new RNA();
-                    Graphics tekenveld = tekenpaneel.getGraphics();
-                    int afstand = 50;
-                    for (int i = 0; i < seq.length(); i++) {
-                        char a = seq.charAt(i);
-                        r1.setcolor(a);
-                        tekenveld.setColor(r1.getColor());
-                        tekenveld.drawRect(afstand, 50, 10, 20);
-                        tekenveld.fillRect(afstand, 50, 10, 20);
-                        afstand += 10;
-                    }
-                } else if (jeiwit == t) {
-                    textArea.setText("Eiwit: " + seq);
-                    Eiwit p1 = new Eiwit();
-                    Graphics tekenveld = tekenpaneel.getGraphics();
-                    int afstand = 50;
-                    for (int i = 0; i < seq.length(); i++) {
-                        char a = seq.charAt(i);
-                        p1.setcolor(a);
-                        tekenveld.setColor(p1.getColor());
-                        tekenveld.drawRect(afstand, 50, 10, 20);
-                        tekenveld.fillRect(afstand, 50, 10, 20);
-                        afstand += 10;
-                    }
-
-                } else {
-                    textArea.setText("Er is een onjuiste sequentie " +
-                            "ingevoerd");
-                }
-
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
+
+            if (jDNA == t) {
+                textArea.setText("DNA: " + seq);
+                DNA d1 = new DNA();
+                assert seq != null;
+                d1.setgcpers(seq);
+                textArea.setText("Het GC-percentage is " + d1.getgcpers() + "%");
+                System.out.println(d1.getgcpers());
+
+                Graphics tekenveld = tekenpaneel.getGraphics();
+                int afstand = 50;
+                for (int i = 0; i < seq.length(); i++) {
+                    char a = seq.charAt(i);
+                    d1.setcolor(a);
+                    tekenveld.setColor(d1.getColor());
+                    tekenveld.drawRect(afstand, 50, 10, 20);
+                    tekenveld.fillRect(afstand, 50, 10, 20);
+                    afstand += 10;
+                }
+            } else if (jRNA == t) {
+                textArea.setText("RNA: " + seq);
+                RNA r1 = new RNA();
+                Graphics tekenveld = tekenpaneel.getGraphics();
+                int afstand = 50;
+                for (int i = 0; i < seq.length(); i++) {
+                    char a = seq.charAt(i);
+                    r1.setcolor(a);
+                    tekenveld.setColor(r1.getColor());
+                    tekenveld.drawRect(afstand, 50, 10, 20);
+                    tekenveld.fillRect(afstand, 50, 10, 20);
+                    afstand += 10;
+                }
+            } else if (jeiwit == t) {
+                textArea.setText("Eiwit: " + seq);
+                Eiwit p1 = new Eiwit();
+                Graphics tekenveld = tekenpaneel.getGraphics();
+                int afstand = 50;
+                for (int i = 0; i < seq.length(); i++) {
+                    char a = seq.charAt(i);
+                    p1.setcolor(a);
+                    tekenveld.setColor(p1.getColor());
+                    tekenveld.drawRect(afstand, 50, 10, 20);
+                    tekenveld.fillRect(afstand, 50, 10, 20);
+                    afstand += 10;
+                }
+
+            } else {
+                textArea.setText("Er is een onjuiste sequentie " +
+                        "ingevoerd");
+            }
+
         });
     }
 
